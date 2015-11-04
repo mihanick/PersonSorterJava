78cb91da4c38a71d452c6191b0a4ef34f046be63
mihanick <mihanick@gmail.com>
2015-11-04 19:38:34 +0600

    Add tests for person comparators

--- PersonSorter/src/test/java/edu/omgtu/personsorter/PersonComparatorTest.java
+++ PersonSorter/src/test/java/edu/omgtu/personsorter/PersonComparatorTest.java
@@ -12,17 +12,18 @@ import static org.junit.Assert.*;
  *
  * @author gladkikh
  */
-public class PersonComparerTest {
+public class PersonComparatorTest {
 
-    public PersonComparerTest() {
+    public PersonComparatorTest() {
     }
 
     @Test
     public void TestNameCompareNull() throws Exception {
         Person personLeft = new Person();
         Person personRight = new Person("Бунин Иван РН-231");
+        PersonComparatorByName comparator = new PersonComparatorByName();
         assertEquals("Не выполнено сравнение по имени с пустым Person",
-                -1, PersonComparer.compareByName(personLeft, personRight));
+                -1, comparator.compare(personLeft, personRight));
 
     }
 
@@ -30,32 +31,38 @@ public class PersonComparerTest {
     public void TestNameCompare() throws Exception {
         Person personLeft = new Person("Мандельштам Осип РН-231");
         Person personRight = new Person("Бунин Иван РН-231");
+        PersonComparatorByName comparator = new PersonComparatorByName();
         assertEquals("Не выполнено сравнение по имени с равными группами",
-                1, PersonComparer.compareByName(personLeft, personRight));
+                1, comparator.compare(personLeft, personRight));
     }
 
     @Test
     public void TestGroupNameSurnameCompareDifferentGroup() throws Exception {
         Person personLeft = new Person("Бунин Иван РН-231");
         Person personRight = new Person("Бунин Иван РН-131");
+        PersonComparatorByGroupNameSurname comparator
+                = new PersonComparatorByGroupNameSurname();
         assertEquals("Не выполнено полное сравнение с разными группами",
-                1, PersonComparer.compareByGroupNameSurname(personLeft, personRight));
+                1, comparator.compare(personLeft, personRight));
     }
 
     @Test
     public void TestGroupNameSurnameCompareSameGroup() throws Exception {
         Person personLeft = new Person("Мандельштам Осип РН-231");
         Person personRight = new Person("Бунин Иван РН-231");
+        PersonComparatorByGroupNameSurname comparator
+                = new PersonComparatorByGroupNameSurname();
         assertEquals("Не выполнено полное сравнение с одинаковыми группами",
-                1, PersonComparer.compareByGroupNameSurname(personLeft, personRight));
+                1, comparator.compare(personLeft, personRight));
     }
 
     @Test
     public void TestGroupNameSurnameCompareSameGroupName() throws Exception {
         Person personLeft = new Person("Некрасов Иван РН-231");
         Person personRight = new Person("Бунин Иван РН-231");
+        PersonComparatorByGroupNameSurname comparator
+                = new PersonComparatorByGroupNameSurname();
         assertEquals("Не выполнено полное сравнение с разными фамилиями",
-                1, PersonComparer.compareByGroupNameSurname(personLeft, personRight));
+                1, comparator.compare(personLeft, personRight));
     }
-
 }

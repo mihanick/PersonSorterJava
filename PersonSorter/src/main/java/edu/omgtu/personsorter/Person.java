1ba610e88e4cdbc44cb423905d78853b9da0409f
mihanick <mihanick@gmail.com>
2015-11-04 18:26:40 +0600

    Add Person class

--- PersonSorter/src/main/java/edu/omgtu/personsorter/Person.java
+++ PersonSorter/src/main/java/edu/omgtu/personsorter/Person.java
@@ -10,5 +10,37 @@ package edu.omgtu.personsorter;
  * @author gladkikh
  */
 public class Person {
+        public String Name;
+
+        public String Surname;
+
+        public String Group;
+
+        /**
+         * Инициализирует пустого человека без имени
+         */
+        public Person()
+        {
+            this.Name = "";
+            this.Surname = "";
+            this.Group = "";
+        }
+
+        public Person(String surnameNameGroup) throws Exception
+        {
+            String[] splittedStrings = surnameNameGroup.split(" ", 0);
+            if (splittedStrings.length !=3)
+                throw new Exception("Неверный формат текстового файла");
+
+            this.Surname = splittedStrings[0];
+            this.Name = splittedStrings[1];
+            this.Group = splittedStrings[2];
+        }
+
+        @Override
+        public String toString()
+        {
+            return this.Name+" "+this.Surname+" "+this.Group;
+        }
     
 }

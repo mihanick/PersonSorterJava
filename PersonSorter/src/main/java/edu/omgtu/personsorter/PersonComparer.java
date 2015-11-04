4d93f2a2c9296b23044c2c3f8a205f96f399ee62
mihanick <mihanick@gmail.com>
2015-11-04 18:42:10 +0600

    Add class stub for implementing comparison

---
+++ PersonSorter/src/main/java/edu/omgtu/personsorter/PersonComparer.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.omgtu.personsorter;
+
+/**
+ *
+ * @author gladkikh
+ */
+    public class PersonComparer
+    {
+        public static int compareByName(Person leftPerson, Person rightPerson) throws Exception
+        {
+            //TODO: Добавить реализацию сравнения по именам
+            throw new Exception("Не реализовано");
+        }
+
+        public static int compareBySurname(Person leftPerson, Person rightPerson) throws Exception
+        {
+            //TODO: Добавить реализацию сравнения по именам
+            throw new Exception("Не реализовано");
+        }
+
+        public static int compareByGroupNameSurname(Person leftPerson, Person rightPerson) throws Exception
+        {
+            //TODO: Добавить реализацию сравнения по именам
+            throw new Exception("Не реализовано");
+        }
+    }

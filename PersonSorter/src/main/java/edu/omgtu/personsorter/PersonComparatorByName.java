3a9991b4ebaef30e73e46d41f6192938c204639b
mihanick <mihanick@gmail.com>
2015-11-04 19:38:09 +0600

    Add classes stubs for comparators of persons by name, surname, group+name+surname

---
+++ PersonSorter/src/main/java/edu/omgtu/personsorter/PersonComparatorByName.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.omgtu.personsorter;
+
+import java.util.Comparator;
+
+/**
+ *
+ * @author gladkikh
+ */
+    public class PersonComparatorByName implements Comparator<Person>
+    {
+        @Override
+        public int compare(Person leftPerson, Person rightPerson) 
+        {
+            //TODO: Добавить реализацию сравнения по именам
+            return 0;
+        }
+    }
